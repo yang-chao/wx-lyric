0cc74b13c6aadf5fa397db18712589c9d2c47e47
杨超 <y.yangchao@gmail.com>
2018-06-13 14:39:10 +0800

    增加第一个文件

---
+++ lyric.py
@@ -0,0 +1,40 @@
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+	username="root",
+	password="root",
+	hostname="140.143.12.108",
+	databasename="music")
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Music(db.Model):
+	__tablename__= 'music'
+
+	id = db.Column(db.String(32), primary_key=True)
+	name = db.Column(db.String(1000))
+	artist = db.Column(db.String(30))
+	style = db.Column(db.String(20))
+	language = db.Column(db.String(10))
+
+class Lyric(db.Model):
+	__tablename__= 'lyric'
+	id = db.Column(db.Integer, primary_key=True)
+	music_id = db.Column(db.String(32))
+	language = db.Column(db.String(1000))
+
+
+@app.route("/music", methods=['GET'])
+def get_all_music():
+	musicList = db.session.query(Music).all()
+    return str(rv)
+
+
+if __name__ == "__main__":
+    app.run()
