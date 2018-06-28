3afeaea83b5c1fd9af02ce7361702a26eb2782a4
ubuntu <ubuntu@localhost.localdomain>
2018-06-28 23:33:29 +0800

    add topic api

--- lyric.py
+++ lyric.py
@@ -1,103 +1,180 @@
-#coding:utf-8
+# coding:utf-8
 from flask import Flask, Response, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from spider import spider
-import json, md5
+import json
+import md5
+import random
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-	username="root",
-	password="root",
-	hostname="140.143.12.108",
-	databasename="music")
+    username="root",
+    password="root",
+    hostname="140.143.12.108",
+    databasename="music")
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Artist(db.Model):
-	__tablename__= 'artist'
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(20))
-	country = db.Column(db.String(10))
-	sex = db.Column(db.String(1))
+class Artist(db.Model):
+    __tablename__ = 'artist'
+
+    _id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
+    name = db.Column(db.String(20))
+    avatar = db.Column(db.String(200))
+    kind = db.Column(db.String(7))
+    sex = db.Column(db.String(1))
+
 
 class Music(db.Model):
-	__tablename__= 'music'
+    __tablename__ = 'music'
 
-	id = db.Column(db.String(32), primary_key=True)
-	name = db.Column(db.String(100))
-	style = db.Column(db.String(10))
-	language = db.Column(db.String(4))
-	album = db.Column(db.String(20))
+    _id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    artist = db.Column(db.String(50))
+    album = db.Column(db.String(20))
+
 
 class Lyric(db.Model):
-	__tablename__= 'lyric'
+    __tablename__ = 'lyric'
 
-	id = db.Column(db.Integer, primary_key=True)
-	music_id = db.Column(db.String(32))
-	artist_id = db.Column(db.Integer)
-	sentence = db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    music_id = db.Column(db.String(32))
+    artist_id = db.Column(db.Integer)
+    sentence = db.Column(db.String(200))
 
 
 @app.route("/music", methods=['GET'])
 def get_all_music():
-	musicArray = []
-	lyricList = db.session.query(Lyric).all()
-	for lyric in lyricList:
-		musicList = db.session.query(Music).filter(Music.id==lyric.music_id).all()
-		if musicList and musicList[0]:
-			musicObj = {}
-			musicObj['id'] = lyric.id
-			musicObj['lyric'] = lyric.sentence
-			musicObj['name'] = musicList[0].name
-			musicObj['artist'] = musicList[0].artist
-			musicObj['style'] = musicList[0].style
-			musicObj['language'] = musicList[0].language
-			musicArray.append(musicObj)
-	return Response(json.dumps(musicArray), mimetype="text/json")
+    musicArray = []
+    lyricList = db.session.query(Lyric).all()
+    for lyric in lyricList:
+        musicList = db.session.query(Music).filter(
+            Music.id == lyric.music_id).all()
+        if musicList and musicList[0]:
+            musicObj = {}
+            musicObj['id'] = lyric.id
+            musicObj['lyric'] = lyric.sentence
+            musicObj['name'] = musicList[0].name
+            musicObj['artist'] = musicList[0].artist
+            musicObj['style'] = musicList[0].style
+            musicObj['language'] = musicList[0].language
+            musicArray.append(musicObj)
+    return Response(json.dumps(musicArray), mimetype="text/json")
+
 
 @app.route("/music/add", methods=['GET', 'POST'])
 def add_music():
-	if request.method == 'GET':
-		return render_template('music_add.html')
-	else:
-		musics = request.form['musics']
-		result = {}
-		if musics:
-			lines = musics.split('\n')
-			for line in lines:
-				print('line: ' + line)
-				elements = line.split('|')
-				m = md5.new()
-				m.update((elements[0] + '+' + elements[1]).encode(encoding='utf-8'))
-				id = m.hexdigest()
-				print('id: ' + id)
-				sentence = elements[4]
-				print('sentence: ' + sentence)
+    if request.method == 'GET':
+        return render_template('music_add.html')
+    else:
+        musics = request.form['musics']
+        result = {}
+        if musics:
+            lines = musics.split('\n')
+            for line in lines:
+                print('line: ' + line)
+                elements = line.split('|')
+                m = md5.new()
+                m.update((elements[0] + '+' + elements[1]
+                          ).encode(encoding='utf-8'))
+                id = m.hexdigest()
+                print('id: ' + id)
+                sentence = elements[4]
+                print('sentence: ' + sentence)
 
-				# insert into music
-				music = Music(id=id, name=elements[0], artist=elements[1], style=elements[2],
-					language=elements[3])
-				db.session.merge(music)
+                # insert into music
+                music = Music(id=id, name=elements[0], artist=elements[1], style=elements[2],
+                              language=elements[3])
+                db.session.merge(music)
 
-				# insert into lyric
-				lyric = Lyric(music_id=id, sentence=sentence.replace('\r',''))
-				db.session.add(lyric)
+                # insert into lyric
+                lyric = Lyric(music_id=id, sentence=sentence.replace('\r', ''))
+                db.session.add(lyric)
 
-			db.session.commit()
-			db.session.close()
-			result['code'] = 0
-		else:
-			result['code'] = 1
-		return Response(json.dumps(result), mimetype="text/json")
+            db.session.commit()
+            db.session.close()
+            result['code'] = 0
+        else:
+            result['code'] = 1
+        return Response(json.dumps(result), mimetype="text/json")
 
-@app.route('/artist/add')
+
+@app.route('/spider/artist/add')
 def insertArtists():
-	artists = spider.fetchArtists()
-	json.dumps(artists)
+    ids = [1001, 1002, 1003, 2001, 2002, 2003]
+    kind = ['华语', '华语', '华语', '欧美', '欧美', '欧美']
+    sex = ['男', '女', '组合', '男', '女', '组合']
+    for index in range(len(ids)):
+        artists = spider.fetchArtists(ids[index])['artists']
+        for artist in artists:
+            newArtist = Artist(
+                id=artist['id'], name=artist['name'], avatar=artist['img1v1Url'],
+                kind=kind[index], sex=sex[index])
+            db.session.add(newArtist)
+    db.session.commit()
+    db.session.close()
+    return Response(json.loads('{"code": 0}'), mimetype="text/json")
+
+
+@app.route('/spider/music/add')
+def insertMusics():
+    allArtist = db.session.query(Artist).offset(100).limit(100).all()
+    for artist in allArtist:
+        hotMusics = spider.fetchMusics(artist.id)
+        for music in hotMusics:
+            newMusic = Music(id=music['id'], name=music['name'],
+                             artist=artist.name, album=music['al']['name'])
+            db.session.add(newMusic)
+    db.session.commit()
+    db.session.close()
+    return Response(json.loads('{"code": 0}'), mimetype="text/json")
+
+
+@app.route('/topic')
+def getTopic():
+    # 随机选取一个歌词
+    sql = "SELECT * FROM {table} AS t1 JOIN (SELECT ROUND(RAND() * ((SELECT MAX(id) FROM {table})-(SELECT MIN(id) FROM {table}))+(SELECT MIN(id) FROM {table})) AS id) AS t2 WHERE t1.id >= t2.id ORDER BY t1.id LIMIT 1;".format(table='lyric')
+    result = db.engine.execute(sql)
+    for r in result:
+        print(r.sentence)
+    topic = {}
+    if r:
+        # 查询歌曲
+        musicResult = db.session.query(Music).filter(
+            Music.id == r.music_id).first()
+
+        # 查询歌手
+        artistResult = db.session.query(Artist).filter(
+            Artist.id == r.artist_id).first()
+
+        # 随机查询歌手的其它歌曲
+        answer = []
+        connection = db.engine.connect()
+        for index in range(3):
+            musicSql = u"SELECT * FROM {table} AS t1 JOIN (SELECT ROUND(RAND() * ((SELECT MAX(id) FROM {table})-(SELECT MIN(id) FROM {table}))+(SELECT MIN(id) FROM {table})) AS id) AS t2 WHERE t1.id >= t2.id AND t1.artist = '{artist}' ORDER BY t1.id LIMIT 1;".format(
+                table='music',
+                artist=artistResult.name)
+            randomMusic = connection.execute(musicSql).first()
+            answer.append(randomMusic.name)
+        
+        correctIndex = random.randint(0, 3)
+        answer.insert(correctIndex, musicResult.name)
+
+        topic['question'] = r.sentence
+        topic['answer'] = answer
+        topic['correctAnswer'] = correctIndex
+        topic['type'] = 1
+        topic['artist'] = artistResult.name
+    else:
+        topic['error'] = 1
+    return Response(json.dumps(topic), mimetype="text/json")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
--- spider/spider.py
+++ spider/spider.py
@@ -1,22 +1,27 @@
-#coding:utf-8
-import requests, json
+# coding:utf-8
+import requests
+import json
 
-url = 'http://music.163.com/api/artist/list?offset=0&limit=100&categoryCode=1002'
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Host': 'music.163.com',
-    'Pragma': 'no-cache',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-    'Referer': 'http://music.163.com/',
-    'Cookie': 'vjuids=bb2f71c7.13c8fd58e63.0.526055ab; ALLYESID4=00130209142213933388358; pgv_pvi=9704409088; SID=99e269cd-97df-44c6-b36e-56ebd2550e83; hasPay=0; mail_psc_fingerprint=1839500596; _ga=GA1.2.126106441.1448683715; _ntes_nnid=fa0bc4b4c44a218dcdc5eb69a7f0ba39,1499650656366; _ntes_nuid=fa0bc4b4c44a218dcdc5eb69a7f0ba39; SERVER_ID=90; TOKEN=bFdKWKC8kDJRlLbt; __utma=187553192.126106441.1448683715.1500357846.1500369700.3; __utmc=187553192; NTES_CMT_USER_INFO=4112738%7Clongsurvivor%7Chttps%3A%2F%2Fsimg.ws.126.net%2Fe%2Fcms-bucket.nosdn.127.net%2F465df94e2fae4f279ba8cab0f4418e0320170801165704.jpg.39x39.100.jpg%7Cfalse%7CeWFuZ2NoYW8yOTRAMTI2LmNvbQ%3D%3D; vjlast=1359622803.1505297203.12; ne_analysis_trace_id=1505297203110; vinfo_n_f_l_n3=e5655f61fe829491.1.30.1412149722731.1449288878286.1505297209174; s_n_f_l_n3=e5655f61fe8294911449310040130; Qs_lvt_73318=1504744805%2C1505265948%2C1509798854; Qs_pv_73318=3600702246289897500%2C2302498089758868500%2C1271704622791393000%2C1066600583527749900; _jzqa=1.3370063108192486000.1526891362.1526891362.1526891362.1; _jzqc=1; _jzqy=1.1526891362.1526891362.1.jzqsr=baidu.-; __f_=1526891362536; PRINTUPLOADID=DC346017EE0FDC6DC93FF02011A0DEA0.app-54-28009; usertrack=ezq0pVsfaMt199upC0bhAg==; Province=010; City=010; CIRCLETRACK=172.17.0.75.1528785402822437; starttime=; NTES_SESS=Qn4Q0hA_xgke1VpsTVEDE7qeB5_57bHvqxd.ti.9XWs_MqYXamwM5Xidb.NUaqcBl9q2Ix9Qqm0LTyziTn8tGgpNcaOEDQMP7lD6GcHMqGKpWsl7IHjrnoGp3KHfiTzUKOjxl70rWnqIGALys.kKBe244vEZPqL47AT_kTxT_GFrBA6QWc.DjUDQllxd3D7WF; S_INFO=1528785739|0|3&40##|y_yangchao#y_yangchao@126.com#yangchao294@126.com; P_INFO=y_yangchao@163.com|1528785739|0|mail163|11&18|bej&1527316935&yxp#bej&null#10#0#0|&0|yxp|y_yangchao@163.com; nts_mail_user=y_yangchao@163.com:-1:1; df=mail163_letter; MUSIC_EMAIL_U=138ea797cef4013587a93d8c90bc84175956e2c62ec1556983a3d28e98c03e3c261fb260c2d1e133a56edab6c0e405500aab8dee2285404b8bafcdfe5ad2b092; _iuqxldmzr_=32; playliststatus=visible; oulink_h=779; __utmc=94650624; __utma=94650624.126106441.1448683715.1529481369.1529487199.3; __utmz=94650624.1529487199.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); JSESSIONID-WYYY=IApiDk3tMhSwtVqSNmy%5C4Y1V770GuN2xY%5CFME8GB2DvC5mMN9eUHMbIKcHjo8aza9UqfKnJGicxY2HrAYzAj6%2Fp9n%5CiWszJWfXcg9sPmVwBNo3Xw6mPnYbvHvvJP%2FI632FH7dweUDkSDat2%2B635bVyWR%5CT2rhekdUHo6nas04uzZH6tc%3A1529492531360; WM_TID=6XbF3ccy%2FqP0vC4WToVJ24Bwqwa%2FmONV; MUSIC_U=8b06274f4b6251d1cff54adc48d0ae04708ec92d197a695671736d3e61e2e1c3cb6cf4fdb8e45e8c49ec8eb1624cb4198bafcdfe5ad2b092; __remember_me=true; __csrf=888d0de9fac161858113bee264e4b36a; __utmb=94650624.26.10.1529487199'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'music.163.com',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+        'Referer': 'http://music.163.com/',
+        'Cookie': 'vjuids=bb2f71c7.13c8fd58e63.0.526055ab; ALLYESID4=00130209142213933388358; pgv_pvi=9704409088; SID=99e269cd-97df-44c6-b36e-56ebd2550e83; hasPay=0; mail_psc_fingerprint=1839500596; _ga=GA1.2.126106441.1448683715; _ntes_nnid=fa0bc4b4c44a218dcdc5eb69a7f0ba39,1499650656366; _ntes_nuid=fa0bc4b4c44a218dcdc5eb69a7f0ba39; SERVER_ID=90; TOKEN=bFdKWKC8kDJRlLbt; __utma=187553192.126106441.1448683715.1500357846.1500369700.3; __utmc=187553192; NTES_CMT_USER_INFO=4112738%7Clongsurvivor%7Chttps%3A%2F%2Fsimg.ws.126.net%2Fe%2Fcms-bucket.nosdn.127.net%2F465df94e2fae4f279ba8cab0f4418e0320170801165704.jpg.39x39.100.jpg%7Cfalse%7CeWFuZ2NoYW8yOTRAMTI2LmNvbQ%3D%3D; vjlast=1359622803.1505297203.12; ne_analysis_trace_id=1505297203110; vinfo_n_f_l_n3=e5655f61fe829491.1.30.1412149722731.1449288878286.1505297209174; s_n_f_l_n3=e5655f61fe8294911449310040130; Qs_lvt_73318=1504744805%2C1505265948%2C1509798854; Qs_pv_73318=3600702246289897500%2C2302498089758868500%2C1271704622791393000%2C1066600583527749900; _jzqa=1.3370063108192486000.1526891362.1526891362.1526891362.1; _jzqc=1; _jzqy=1.1526891362.1526891362.1.jzqsr=baidu.-; __f_=1526891362536; PRINTUPLOADID=DC346017EE0FDC6DC93FF02011A0DEA0.app-54-28009; usertrack=ezq0pVsfaMt199upC0bhAg==; Province=010; City=010; CIRCLETRACK=172.17.0.75.1528785402822437; starttime=; NTES_SESS=Qn4Q0hA_xgke1VpsTVEDE7qeB5_57bHvqxd.ti.9XWs_MqYXamwM5Xidb.NUaqcBl9q2Ix9Qqm0LTyziTn8tGgpNcaOEDQMP7lD6GcHMqGKpWsl7IHjrnoGp3KHfiTzUKOjxl70rWnqIGALys.kKBe244vEZPqL47AT_kTxT_GFrBA6QWc.DjUDQllxd3D7WF; S_INFO=1528785739|0|3&40##|y_yangchao#y_yangchao@126.com#yangchao294@126.com; P_INFO=y_yangchao@163.com|1528785739|0|mail163|11&18|bej&1527316935&yxp#bej&null#10#0#0|&0|yxp|y_yangchao@163.com; nts_mail_user=y_yangchao@163.com:-1:1; df=mail163_letter; MUSIC_EMAIL_U=138ea797cef4013587a93d8c90bc84175956e2c62ec1556983a3d28e98c03e3c261fb260c2d1e133a56edab6c0e405500aab8dee2285404b8bafcdfe5ad2b092; _iuqxldmzr_=32; playliststatus=visible; oulink_h=779; __utmc=94650624; __utma=94650624.126106441.1448683715.1529481369.1529487199.3; __utmz=94650624.1529487199.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); JSESSIONID-WYYY=IApiDk3tMhSwtVqSNmy%5C4Y1V770GuN2xY%5CFME8GB2DvC5mMN9eUHMbIKcHjo8aza9UqfKnJGicxY2HrAYzAj6%2Fp9n%5CiWszJWfXcg9sPmVwBNo3Xw6mPnYbvHvvJP%2FI632FH7dweUDkSDat2%2B635bVyWR%5CT2rhekdUHo6nas04uzZH6tc%3A1529492531360; WM_TID=6XbF3ccy%2FqP0vC4WToVJ24Bwqwa%2FmONV; MUSIC_U=8b06274f4b6251d1cff54adc48d0ae04708ec92d197a695671736d3e61e2e1c3cb6cf4fdb8e45e8c49ec8eb1624cb4198bafcdfe5ad2b092; __remember_me=true; __csrf=888d0de9fac161858113bee264e4b36a; __utmb=94650624.26.10.1529487199'
 }
 
-def fetchArtists():
+def fetchArtists(id):
+    url = 'http://music.163.com/api/artist/list?offset=0&limit=100&categoryCode={}'.format(id)
     r = requests.get(url, headers=headers)
-    artists = json.loads(r.content)
-    return artists
+    return json.loads(r.content)
+
+def fetchMusics(artistId):
+    url = 'http://music.163.com/api/v1/artist/{}'.format(artistId)
+    r = requests.get(url, headers=headers)
+    return json.loads(r.content)['hotSongs']
